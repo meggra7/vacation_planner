09575d0fee4a10287d766bc871a0ffe14cff3db5
Megs <meghan.g.dunham@gmail.com>
2020-07-30 18:08:10 -0400

    Style card for each trip entry

--- README.md
+++ README.md
@@ -34,3 +34,7 @@ Service workers have been added to cache website data for improved performance i
 
 ## Authors and acknowledgment
 All code (HTML, CSS, JavaScript) original by Meghan Dunham.  APIs used include Geonames (verify location), WeatherBit (weather forecasts), and Pixabay (images).
+
+
+Luggage photo attribution:
+Image by <a href="https://pixabay.com/users/stux-12364/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1149289">Stux</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1149289">Pixabay</a>
--- src/client/index.js
+++ src/client/index.js
@@ -6,19 +6,20 @@ import './styles/reset.scss';
 import './styles/base.scss';
 import './styles/header.scss';
 import './styles/form.scss';
+import './styles/entry.scss';
 
 /* Import and set image(s) */
 import loadingIndicator from './assets/Cube-1s-200px.gif';
+import placeholderImage from './assets/luggage.jpg';
 import stepUpcoming from './assets/circle_outline_32dp_primary_dark.png';
 import stepCompleted from './assets/circle_filled_32dp_primary_dark.png';
 import airplane from './assets/airplane-32dp-777.png';
 
 
 document.getElementById('step-one').setAttribute('src', stepCompleted);
-
 document.getElementById('step-two').classList.add('current');
 document.getElementById('step-two').setAttribute('src', airplane);
-
 document.getElementById('step-three').setAttribute('src', stepUpcoming);
-
 document.getElementById('step-four').setAttribute('src', stepUpcoming);
+
+document.getElementById('upcoming').getElementsByTagName('img')[0].setAttribute('src', placeholderImage);

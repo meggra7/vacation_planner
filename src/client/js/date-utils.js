72c774d488d91395accff21077241b7355fbdcfb
Megs <meghan.g.dunham@gmail.com>
2020-08-04 00:41:42 -0400

    refactor: Remove leading zeros for date trip display

--- src/client/js/date-utils.js
+++ src/client/js/date-utils.js
@@ -46,7 +46,11 @@ function getLongDate(date) {
 
     const year = date.slice(0,4);
     const monthNumber = date.slice(5,7);
-    const day = date.slice(8);
+    let day = date.slice(8);
+    if (day.slice(0,1) === '0') {
+        // Remove any leading zeros
+        day = day.slice(1);
+    }
 
     let monthWord = '';
     switch (monthNumber) {
